455835f8c74a8ef0f85e6cfb8ccc332e58a8732e
Suzy Burgum <suzyburgum@gmail.com>
2024-04-23 15:07:36 -0400

    add salt and hash encryption to register route

--- main.py
+++ main.py
@@ -34,10 +34,15 @@ def home():
 @app.route('/register', methods = ["GET", "POST"])
 def register():
     if request.method == "POST":
+        hash_and_salt_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
         new_user= User(
             email=request.form.get('email'),
             name=request.form.get('name'),
-            password=request.form.get('password')
+            password=hash_and_salt_password,
         )
         db.session.add(new_user)
         db.session.commit()

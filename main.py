1db34653e31b3057106ca9b0214d58a30e85db79
Suzy Burgum <suzyburgum@gmail.com>
2024-04-23 15:37:52 -0400

    config flask-login manager create a user loader callback add UserMixin to DB creation add login_user(new_user) to authenticate update login route & secrets route only logged in users can access file

--- main.py
+++ main.py
@@ -8,20 +8,35 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
+
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-# CREATE TABLE IN DB
-class User(db.Model):
+#CONFIGURE FLASK-LOGIN'S LOGIN MANAGER
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+#CREATE A USER_LOADER CALLBACK
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
+# CREATE TABLE IN DB w/ UserMixin
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
- 
+
+
 with app.app_context():
     db.create_all()
 
@@ -31,42 +46,65 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register', methods = ["GET", "POST"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        hash_and_salt_password = generate_password_hash(
+        hash_and_salted_password = generate_password_hash(
             request.form.get('password'),
             method='pbkdf2:sha256',
             salt_length=8
         )
-        new_user= User(
+
+        new_user = User(
             email=request.form.get('email'),
             name=request.form.get('name'),
-            password=hash_and_salt_password,
+            password=hash_and_salted_password,
         )
+
         db.session.add(new_user)
         db.session.commit()
-        #passing over the user's name w
+
+        #login and authenticate user after adding detail to database:
+        login_user(new_user)
+        #can redirect and get the name from the current use
         return render_template("secrets.html", name=request.form.get('name').capitalize())
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods = ["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        #find user by email
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+
+        #check stored password hash against entered password hashed
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('secrets.html'))
+
     return render_template("login.html")
 
-
+#only logged-in users can access route
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    print(current_user.name)
+    #passing the name from the current user
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
-
+#only logged in users can download the document
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory('static', path="files/cheat_sheet.pdf")
 
